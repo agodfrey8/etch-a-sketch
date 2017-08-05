2ff1fb84c3bb56e19c6edcdba2a5fc912d85c6a4
Austin Godfrey <bondboy88@gmail.com>
2017-08-06 00:19:12 +0900

    Finish set button

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,34 @@
 $(document).ready(function(){
-    var rowMax = 96;
-    var colMax = 96;
+  var startGridSize = 16;
+  var newGridSize = startGridSize;
+  buildGrid(startGridSize);
+
+  $("#set-btn").click(function(){
+    newGridSize = prompt("Enter new grid size", newGridSize);
+    buildGrid(newGridSize);
+  });
+
+  function buildGrid(grid){
+    var rowLimit = grid;
+    var colLimit = grid;
+
+    $(".container").remove();
+
     $("body").append('<div class="container">');
-    for(var i = 0; i < rowMax; i++){
+    for(var i = 0; i < rowLimit; i++){
       $(".container").append('<div class="row" id="row' + i + '">');
-      for (var j = 0; j < colMax; j++){
+      for (var j = 0; j < colLimit; j++){
         $("#row" + i).append('<div class="square" id ="square' + i + j + '">');
       }
     }
-    $(".square").hover(function(){
-        $(this).css("background-color", "grey");
+
+    $(".square").on( "mouseenter", function(){
+      $(this).css("background-color", "grey");
     });
+
+    var squareWidth = $(".container").width()/grid;
+    var squareHeight = $(".container").height()/grid;
+    $(".square").css("width", squareWidth + "px");
+    $(".row").css("height", squareHeight + "px");
+  }
 });
